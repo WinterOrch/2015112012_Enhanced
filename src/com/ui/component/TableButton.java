d341015e944568ef1fc35bbe3c22f42d57fe4849
Yuan.TX <yuantianxin@126.com>
2018-07-18 16:35:53 +0800

    <ADD> - Button Used In Tables

---
+++ src/com/ui/component/TableButton.java
@@ -0,0 +1,34 @@
+package com.ui.component;
+
+import com.system.PropertiesLocale;
+import com.ui.constant.UIConstant;
+
+import javax.swing.*;
+
+public class TableButton extends JButton{
+    private int row;
+    private int column;
+
+    public TableButton() {
+        super(PropertiesLocale.getProperty("UI.KDC.GET"), UIConstant.ICON_GET);
+        this.setPreferredSize(UIConstant.LABLE_SIZE_ITEM);
+        this.setFont(UIConstant.FONT_BUTTON);
+        this.setFocusable(false);
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    public int getColumn() {
+        return column;
+    }
+
+    public void setColumn(int column) {
+        this.column = column;
+    }
+}
